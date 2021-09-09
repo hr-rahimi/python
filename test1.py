b5ed336f5d1dc0958d601b391174997e6050c207
hr-rahimi <hr.rahimi92@gmail.com>
2021-09-09 16:40:29 +0430

    5*5 Table

--- test1.py
+++ test1.py
@@ -1,11 +1,22 @@
-print("I'm Here Now ...")
+# print("I'm Here Now ...")
 
 
-degree = int(input("Please Enter Your Water Degree ! \n\t>_")) 
-if degree > 100:
-   print("Stem")
-elif(degree < 0):
-   print("Ice")
-else:
-    print("Water")
+# degree = int(input("Please Enter Your Water Degree ! \n> ")) 
+# if degree > 100:
+#    print("\nStem")
+# elif(degree < 0):
+#    print("\nIce")
+# else:
+#     print("\nWater")
     
+    
+# 1 2 3 4 5
+# 2
+# 3
+# 4
+# 5
+
+for row in range(1,6):
+    for col in range(1,6):
+            print(row*col , end=' ' )
+    print(' ')
