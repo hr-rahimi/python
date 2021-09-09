e2f4608a419af18fe9924d3374a3952d838b8578
hr-rahimi <hr.rahimi92@gmail.com>
2021-09-09 15:15:12 +0430

    New Python App In Command

--- test1.py
+++ test1.py
@@ -1,1 +1,11 @@
 print("I'm Here Now ...")
+
+
+degree = int(input("Please Enter Your Water Degree ! \n\t>_")) 
+if degree > 100:
+   print("Stem")
+elif(degree < 0):
+   print("Ice")
+else:
+    print("Water")
+    
